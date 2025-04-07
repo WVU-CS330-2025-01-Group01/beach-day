de9f2b3f6b1d037bf211ce023dcaa0167ad96b2f
Bhavana Dakshinamoorthy <bhavana@Bhavanas-MacBook-Air-270.local>
2025-04-07 15:43:03 -0400

    Adds new functions for storing favorite beaches: removeFavorites() and getFavorites()

--- backend/dbabs/index.js
+++ backend/dbabs/index.js
@@ -7,13 +7,10 @@ class UserAlreadyExists extends Error {}
 class ProblemWithDB extends Error {}
 class UserNotFound extends Error {}
 class IncorrectPassword extends Error {}
+class BeachNotPresent extends Error {}
 
 
-// process.on('uncaughtException', (err) => {
-//     console.error('Uncaught Exception:', err);
-	
-// });
-
+console.log(process.env.BEACH_DAY_DB_NAME);
 const connection = mysql.createPool({
 	host: process.env.BEACH_DAY_DB_HOST,    
 	user: process.env.BEACH_DAY_DB_USER,    
@@ -25,6 +22,22 @@ const connection = mysql.createPool({
   	queueLimit: 0
 }).promise();
 
+async function testDatabaseConnection() {
+    let connect;
+    try {
+      connect = await connection.getConnection(); // Get a connection from the pool
+    } catch (err) {
+      console.error('Error connecting to the database:', err.message || err);
+      throw err; // Exit if there’s an error, no need to release connection
+    } finally {
+      if (connect) {
+        connect.release(); // Release the connection back to the pool
+      }
+    }
+  }
+  
+  testDatabaseConnection();
+
 async function getUserData(username) {
 
 	try {
@@ -35,7 +48,6 @@ async function getUserData(username) {
 					//errorCode = "UserNotFound"
 					throw new UserNotFound();
 				}
-
 		return user[0];
 
 
@@ -103,6 +115,162 @@ function tempInitDB() {}
  * would be best if we put the check in this file and have an Error associated
  * with it. We can discuss that later. Same story for the password.
  */
+
+function validateInputAlphaNumeric(input) {
+    const regex = /^[a-zA-Z0-9_]*$/;
+    return regex.test(input);
+}
+
+async function tryAddFavorites(username, beach) {
+try {
+
+const user = await getUserData(username);
+
+if(!validateInputAlphaNumeric(beach)) {
+console.log("That input ain't right dawg");
+throw new ProblemWithDB();
+}
+
+if (user.favorite_beaches === "NULL_BEACH") {
+await connection.query(
+`
+UPDATE users
+SET favorite_beaches = ?
+WHERE username = ?;
+`,
+[beach, username]
+);
+return;
+}
+let favBeachesArr = (user.favorite_beaches).split(",");
+let existsFlag = false;
+
+for(i = 0; i < favBeachesArr.length; i++) {
+if(favBeachesArr[i] === beach) {
+existsFlag = true;
+break;
+}
+}
+
+if(!existsFlag) {
+favBeachesArr.push(beach);
+} else {
+//if we want an error for beach already exists
+}
+
+let favBeachesStringCSV = favBeachesArr.join(",");
+//console.log(favBeachesStringCSV);
+
+await connection.query(
+`
+UPDATE users
+SET favorite_beaches = ?
+WHERE username = ?;
+`,
+[favBeachesStringCSV, username]
+);
+
+} catch (e) {
+console.log(e);
+if(e instanceof UserNotFound) {
+throw new UserNotFound();
+} else {
+throw new ProblemWithDB()
+}
+} 
+}
+
+async function functionTester(){
+
+	const [rowsBefore] = await connection.query(
+		`SELECT * FROM users;`
+	)
+	//the 4 is just whichever user I was testing, your index won't match mine
+
+	console.log(rowsBefore[0].favorite_beaches);
+
+
+	//await tryClearFavorites("personal");
+	//await tryAddFavorites("dropThisUser", "\"\".etes\"\"");
+	//await tryAddFavorites("dropThisUser", "beach4");
+	await tryAddFavorites("dropThisUser", "beach4");
+	//await tryClearFavorites("dropThisUser");
+
+	const [rowsAfter] = await connection.query(
+		`SELECT * FROM users;`
+	)
+
+	console.log(rowsAfter[0].favorite_beaches);
+}
+
+//functionTester();
+
+
+async function removeFavorites(username, beach){
+    try {
+    const user = await getUserData(username);
+    
+    if(!validateInputAlphaNumeric(beach)) {
+    console.log("That input ain't right dawg");
+    throw new ProblemWithDB();
+    }
+    
+    if (user.favorite_beaches === "NULL_BEACH") {
+    console.log("No beach found")
+    throw new BeachNotPresent();
+    }
+    let beaches = user.favorite_beaches.split(",");
+    if(!beaches.includes(beach)){
+    //console.log("Beach is not found");
+    throw new BeachNotPresent();
+    //return;
+    }
+    beaches = beaches.filter(beaches => beaches !== beach);
+    if (beaches.length === 0){
+    beaches = ["NULL_BEACH"];
+    }
+    let CSVbeaches = beaches.join(",");
+    await connection.query(
+                `UPDATE users SET favorite_beaches = ? WHERE username = ?;`,[CSVbeaches, username]
+            );
+    } catch (e) {
+    console.log(e);
+    if (e instanceof UserNotFound) {
+    throw new UserNotFound();
+    } else if (e instanceof BeachNotPresent) {
+    throw new BeachNotPresent();
+    }
+    else {
+    throw new ProblemWithDB();
+    }
+    }
+    }
+
+	async function removeTester(){
+
+		const [rowsBefore] = await connection.query(
+			`SELECT * FROM users;`
+		)
+		//the 4 is just whichever user I was testing, your index won't match mine
+	
+		console.log(rowsBefore[0].favorite_beaches);
+	
+	
+		//await tryClearFavorites("personal");
+		//await tryAddFavorites("dropThisUser", "\"\".etes\"\"");
+		//await tryAddFavorites("dropThisUser", "beach4");
+		await removeFavorites("dropThisUser", "beach2");
+		//await tryClearFavorites("dropThisUser");
+	
+		const [rowsAfter] = await connection.query(
+			`SELECT * FROM users;`
+		)
+	
+		console.log(rowsAfter[0].favorite_beaches);
+	}
+	
+	removeTester();
+
 async function getFavorites(username) {
     try{
         const [favoritesColumn] = await connection.query('SELECT favorite_beaches FROM users WHERE username = ?', [username]);
@@ -116,7 +284,7 @@ async function getFavorites(username) {
         }
         return userFavorite.split(',').map(beach => beach.trim()).filter(beach => beach !== ""); //Returns an array with strings that are
 	} catch (e) {
-		//console.log(e);
+		console.log(e);
 		if (e instanceof UserNotFound) {
 			throw new UserNotFound();
 		} else {
@@ -124,19 +292,6 @@ async function getFavorites(username) {
 		}
 	}
 }
-	//TESTING GET FAVORITES FUNCTION
-	/*
-	const username = 'favTest'; // Replace with the actual username
-	getFavorites(username)
-	  .then(favorites => {
-		console.log('User\'s (withfavorite beaches:', favorites); // Logs the favorites to the console
-	  })
-	  .catch(error => {
-		console.error('Error getting favorites:', error); // Handle errors
-	  });
-	*/
-
-
 
 module.exports = {
 	/**
@@ -211,6 +366,69 @@ module.exports = {
 			}
     },
 
+	getFavorites: async function(username) {
+		try{
+			const [favoritesColumn] = await connection.query('SELECT favorite_beaches FROM users WHERE username = ?', [username]);
+			if (favoritesColumn.length === 0) { 
+				throw new UserNotFound; //Throws error if there is no user favorite.
+			}
+			let userFavorite = favoritesColumn[0].favorite_beaches;
+			 
+			if (userFavorite === "NULL_BEACH" || userFavorite.trim() === "") { //Checks for whether userFavorite is null/empty
+				return []; //Returns an empty array
+			}
+			return userFavorite.split(',').map(beach => beach.trim()).filter(beach => beach !== ""); //Returns an array with strings that are
+		} catch (e) {
+			//console.log(e);
+			if (e instanceof UserNotFound) {
+				throw new UserNotFound();
+			} else {
+				throw new ProblemWithDB();
+			}
+		}
+	},
+
+	removeFavorites: async function (username, beach){
+		try {
+		const user = await getUserData(username);
+		
+		if(!validateInputAlphaNumeric(beach)) {
+		console.log("That input ain't right dawg");
+		throw new ProblemWithDB();
+		}
+		
+		if (user.favorite_beaches === "NULL_BEACH") {
+		console.log("No beach found")
+		throw new BeachNotPresent();
+		}
+		let beaches = user.favorite_beaches.split(",");
+		if(!beaches.includes(beach)){
+		//console.log("Beach is not found");
+		throw new BeachNotPresent();
+		//return;
+		}
+		beaches = beaches.filter(beaches => beaches !== beach);
+		if (beaches.length === 0){
+		beaches = ["NULL_BEACH"];
+		}
+		let CSVbeaches = beaches.join(",");
+		await connection.query(
+					`UPDATE users SET favorite_beaches = ? WHERE username = ?;`,[CSVbeaches, username]
+				);
+		} catch (e) {
+		console.log(e);
+		if (e instanceof UserNotFound) {
+		throw new UserNotFound();
+		} else if (e instanceof BeachNotPresent) {
+		throw new BeachNotPresent();
+		}
+		else {
+		throw new ProblemWithDB();
+		}
+	}
+},
+		
+		
 	initDB: tempInitDB,
 	getFavorites: tempGetFavorites,
 	addFavorite: tempAddFavorite,
@@ -219,7 +437,8 @@ module.exports = {
 	UserAlreadyExists: UserAlreadyExists,
 	ProblemWithDB: ProblemWithDB,
 	UserNotFound: UserNotFound,
-	IncorrectPassword: IncorrectPassword
+	IncorrectPassword: IncorrectPassword,
+	BeachNotPresent: BeachNotPresent,
 };
 	/**
 	 * Export all the errors that can be thrown by the exported functions.
