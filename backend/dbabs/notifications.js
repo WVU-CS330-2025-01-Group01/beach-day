399d06a2c3bdc65eb69ac18f474c559c133813c2
Ayden Jones <aydenjones8@yahoo.com>
2025-04-13 22:17:14 -0400

    feat: split dbabs index.js into multiple files. More will be pruned in a later commit.  getNotificationCount() has been added to notifications file.

--- backend/dbabs/notifications.js
+++ backend/dbabs/notifications.js
@@ -1,37 +1,33 @@
 const bcrypt = require('bcryptjs');
 require('dotenv').config();
 const mysql = require('mysql2');
-const error = require("./db-errors");
-
-
-const connection = mysql.createPool({
-    host: process.env.BEACH_DAY_DB_HOST,
-    user: process.env.BEACH_DAY_DB_USER,
-    password: process.env.BEACH_DAY_DB_PASSWORD,
-    database: process.env.BEACH_DAY_DB_NAME,
-    port: process.env.BEACH_DAY_DB_PORT,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-}).promise();
-
-async function testDatabaseConnection() {
-    let connect;
-    try {
-        connect = await connection.getConnection(); // Get a connection from the pool
-    } catch (err) {
-        console.error('Error connecting to the database:', err.message || err);
-        throw err; // Exit if there’s an error, no need to release connection
-    } finally {
-        if (connect) {
-            connect.release(); // Release the connection back to the pool
-        }
-    }
-}
-
-testDatabaseConnection();
+const dbErrors = require('./db-errors');
+const dbHelper = require('./generic-helpers');
+const connection = require('./database-connection');
 
 module.exports = {
+    getNotificationCount: async function(username) {
+        try {
 
-
+            if(!(await dbHelper.userExists(username))) {
+                throw new dbErrors.UserNotFound();
+            }
+    
+            const [notificationCount] = await connection.query(
+                `
+                SELECT COUNT(username) AS amount
+                FROM notifications
+                WHERE username = ?;
+                `
+                , [username]
+            );
+            return notificationCount[0].amount;
+        } catch (e) {
+            if(e instanceof dbErrors.UserNotFound) {
+                throw new dbErrors.UserNotFound();
+            } else {
+                throw new dbErrors.ProblemWithDB()
+            }
+        }
+    }
 };
