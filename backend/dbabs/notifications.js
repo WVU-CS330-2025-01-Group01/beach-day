a1f6e327b6e7d4df7aad7d73abe6f22552264a34
Ayden Jones <aydenjones8@yahoo.com>
2025-04-15 21:29:18 -0400

    feat: functions receivedNotification(), getUserNotificiations(), addNotification(),

--- backend/dbabs/notifications.js
+++ backend/dbabs/notifications.js
@@ -29,5 +29,101 @@ module.exports = {
                 throw new dbErrors.ProblemWithDB()
             }
         }
+    },
+
+    receivedNotification: async function(username, notification_id) {
+        try {
+    
+            if(!(await dbHelper.userExists(username))) {
+                throw new dbErrors.UserNotFound();
+            }
+    
+            const [notificationQuery] = await connection.query(
+                `
+                    SELECT * FROM notifications
+                    WHERE username = ? AND notification_id = ?;
+                `
+                , [username, notification_id]
+            );
+    
+            if(notificationQuery.length <= 0) {
+                throw new dbErrors.ZeroNotifications();
+            }
+    
+            await connection.query(
+                `
+                    UPDATE notifications 
+                    SET wasReceived = 1
+                    WHERE username = ? AND notification_id = ?;
+                `
+                , [username, notification_id]
+            );
+        } catch (e) {
+            if (e instanceof dbErrors.UserNotFound) {
+                throw new dbErrors.UserNotFound();
+            } else if (e instanceof dbErrors.ZeroNotifications){
+                throw new dbErrors.ZeroNotifications();
+            } else {
+                throw new dbErrors.ProblemWithDB()
+            }
+        }
+    },
+
+    getUserNotifications: async function (username) {
+        try {
+    
+            if(!(await dbHelper.userExists(username))) {
+                throw new dbErrors.UserNotFound();
+            }
+    
+            const [notifications] = await connection.query(
+                `
+                    SELECT creation_time, notification_title, message, email, notifications_enabled, notification_id, notifications.username
+                    FROM notifications LEFT JOIN users
+                    ON  notifications.username = users.username
+                    WHERE notifications.username = ? AND wasReceived = 0
+                    ORDER BY creation_time desc;
+                `
+                , [username]
+            );
+            if (notifications.length <= 0) {
+                throw new dbErrors.ZeroNotifications();
+            }
+    
+            return notifications;
+    
+        } catch (e) {
+            if(e instanceof dbErrors.UserNotFound) {
+                throw new dbErrors.UserNotFound();
+            } else if (e instanceof dbErrors.ZeroNotifications) {
+                throw new dbErrors.ZeroNotifications();
+            } else {
+                throw new dbErrors.ProblemWithDB()
+            }
+        }
+    },
+
+    addNotification: async function (username, title, message) {
+        try {
+    
+            if(!(await dbHelper.userExists(username))) {
+                throw new dbErrors.UserNotFound();
+            }
+    
+            await connection.query(
+                `
+                    INSERT INTO notifications (username, notification_title, message)
+                    VALUES  (?, ?, ?);
+                `
+                , [username, title, message]
+            );
+    
+        } catch (e) {
+            if (e instanceof dbErrors.UserNotFound) {
+                throw new dbErrors.UserNotFound();
+            } else {
+                throw new dbErrors.ProblemWithDB()
+            }
+        }
     }
 };
