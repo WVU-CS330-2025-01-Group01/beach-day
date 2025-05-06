87c129de4a8f5a628ed102076c0d46bbf65cc4d0
WilliamSmithWLS <wls00008@mix.wvu.edu>
2025-05-06 18:47:47 -0400

    Changes 'events' to 'username'

--- backend/dbabs/events.js
+++ backend/dbabs/events.js
@@ -33,8 +33,9 @@ async function getUserEventsHelper(username) {
                 WHERE events.username = ?
                 ORDER BY event_time ASC;
             `
-            , [events]
-        ); // This query joins two tables (events and users) and prints out the attributes on the first line.  It is linked by username. It gets every event in the table at username, in order of soonest to happen.
+            , [username]
+        );
+         // This query joins two tables (events and users) and prints out the attributes on the first line.  It is linked by username. It gets every event in the table at username, in order of soonest to happen.
         if (events.length <= 0) {
             throw new dbErrors.ZeroEvents();
         }
