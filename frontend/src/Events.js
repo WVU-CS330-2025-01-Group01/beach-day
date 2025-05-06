1739432e9400a63680899e2e74bfc66bf7ede24d
WilliamSmithWLS <wls00008@mix.wvu.edu>
2025-05-06 17:32:17 -0400

    Events base page created

---
+++ frontend/src/Events.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState, useContext } from 'react';
+import { UserContext } from './UserContext';
+import './Events.css';
+import { API } from './api';
+
+function Events() {
+
+}
+export default Events;
